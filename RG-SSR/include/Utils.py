08ca76d260473faa20abee2adb03d60dd27c1476
GuiyangLiu <47599530+AchaserL@users.noreply.github.com>
2023-04-28 14:50:17 +0800

    Create Utils.py

---
+++ RG-SSR/include/Utils.py
@@ -0,0 +1,80 @@
+import numpy as np
+import scipy.sparse as sp
+import math
+from scipy import spatial
+
+
+def get_adj(e, KG):
+    M = {}
+    for tri in KG:
+        if tri[0] == tri[2]:
+            continue
+        if (tri[0], tri[2]) not in M:
+            M[(tri[0], tri[2])] = 1
+        if (tri[2], tri[0]) not in M:
+            M[(tri[2], tri[0])] = 1
+    row  = []
+    col  = []
+    data = []
+    for key in M:
+        row.append(key[1])
+        col.append(key[0])
+        data.append(M[key])
+    return sp.coo_matrix((data, (row, col)), shape=(e, e))
+
+
+def fun(KG):
+    head = {}
+    cnt = {}
+    for tri in KG:
+        if tri[1] not in cnt:
+            cnt[tri[1]] = 1
+            head[tri[1]] = {tri[0]}
+        else:
+            cnt[tri[1]] += 1
+            head[tri[1]].add(tri[0])
+    r2f = {}
+    for r in cnt:
+        r2f[r] = len(head[r]) / cnt[r]
+    return r2f
+
+
+def ifun(KG):
+    tail = {}
+    cnt = {}
+    for tri in KG:
+        if tri[1] not in cnt:
+            cnt[tri[1]] = 1
+            tail[tri[1]] = {tri[2]}
+        else:
+            cnt[tri[1]] += 1
+            tail[tri[1]].add(tri[2])
+    r2if = {}
+    for r in cnt:
+        r2if[r] = len(tail[r]) / cnt[r]
+    return r2if
+
+
+def get_weighted_adj(e, KG):
+    r2f = fun(KG)
+    r2if = ifun(KG)
+    M = {}
+    for tri in KG:
+        if tri[0] == tri[2]:
+            continue
+        if (tri[0], tri[2]) not in M:
+            M[(tri[0], tri[2])] = max(r2if[tri[1]], 0.3)
+        else:
+            M[(tri[0], tri[2])] += max(r2if[tri[1]], 0.3)
+        if (tri[2], tri[0]) not in M:
+            M[(tri[2], tri[0])] = max(r2f[tri[1]], 0.3)
+        else:
+            M[(tri[2], tri[0])] += max(r2f[tri[1]], 0.3)
+    row = []
+    col = []
+    data = []
+    for key in M:
+        row.append(key[1])
+        col.append(key[0])
+        data.append(M[key])
+    return sp.coo_matrix((data, (row, col)), shape=(e, e))
